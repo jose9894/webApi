ab5be1451f7817b3446dce7e4a22eec0621972f4
chope9894 <jeppejensen82009@gmail.com>
2025-10-20 23:30:02 +0200

    delete

--- MenuApiV2/DTO/OrderInfoDto.cs
+++
@@ -1,8 +0,0 @@
-namespace MenuApiV2.DTO;
-
-public class OrderInfoDto
-{
-    public string mealName { set; get; }
-    public string cookName { set; get; }
-    public int quantity { set; get; }
-}
--- MenuApiV2/DTO/RatingInfoDto.cs
+++
@@ -1,7 +0,0 @@
-namespace MenuApiV2.DTO;
-
-public class RatingInfoDto
-{
-    public string Name { set; get; }
-    public double? Rating { set; get; }
-}
